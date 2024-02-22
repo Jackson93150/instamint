d047ac182c022ea438d4b4a5567b3562a14cca0f
Jackson93150 <jackjack93150@gmail.com>
2024-02-22 20:33:28 +0100

    feat: test pipelines

--- front/src/pages/home/home-page.tsx
+++ front/src/pages/home/home-page.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
+import { HealthCheck } from "@/services";
+
 export const HomePage = () => {
+  useEffect(() => {
+    // Define the URL of your server
+    HealthCheck;
+  }, []);
   return (
     <div>
       <p>React Template test</p>
---
+++ front/src/services/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from "axios";
+
+export const HealthCheck = () => {
+  axios
+    .get(import.meta.env.VITE_BACKEND_URL)
+    .then((response) => {
+      // Handle successful response
+      console.log("Health check successful:", response.data);
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error fetching health check:", error);
+    });
+};
