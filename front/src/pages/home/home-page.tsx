8e3f04c47ae7aff5436b21a80b43ec94d96896db
Jackson93150 <jackjack93150@gmail.com>
2024-02-22 18:54:45 +0100

    feat: add front app + docs

---
+++ front/src/pages/home/home-page.tsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <p>React Template test</p>
+    </div>
+  );
+};
---
+++ front/tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./lib/ui/src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
