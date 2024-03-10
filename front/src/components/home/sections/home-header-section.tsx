336d7a215b5fe8e897c33991ebfcb459f6648c46
Jackson93150 <jackjack93150@gmail.com>
2024-03-10 17:02:08 +0100

    fix: home header responsive

--- front/src/components/home/sections/home-header-section.tsx
+++ front/src/components/home/sections/home-header-section.tsx
@@ -14,17 +14,21 @@ import 'swiper/swiper-bundle.css';
 
 export const HomeHeaderSection = () => {
   return (
-    <div className="flex h-screen w-screen items-center bg-green-100 px-[5%]">
-      <div className="mt-[-10%] flex w-full justify-between">
-        <div className="flex size-fit flex-col">
-          <span className="text-[96px] font-bold leading-none text-white">Welcome To</span>
-          <span className="bg-green-gradient bg-clip-text text-[96px] font-bold leading-none text-transparent">
+    <div className="tablet:items-center flex h-screen w-screen overflow-hidden bg-green-100 px-[5%]">
+      <div className="tablet:flex-row tablet:mt-[-10%] tablet:justify-between mobile:gap-5U mt-[5%] flex w-full flex-col items-center gap-[20vw]">
+        <div className="tablet:items-start flex size-fit flex-col items-center">
+          <span className="tablet:text-[96px] mobile:text-[8vw] text-[10vw] font-bold leading-none text-white">
+            Welcome To
+          </span>
+          <span className="bg-green-gradient tablet:text-[96px] mobile:text-[12vw] bg-clip-text text-[15vw] font-bold leading-none text-transparent">
             Instamint
           </span>
-          <p className="text-[24px] text-white/40">The platform for minting, buying, and selling NFTs</p>
-          <span className="absolute left-[-450px] top-[-400px] size-[500px] rounded-full bg-green-400 blur-[450px]" />
+          <p className="tablet:text-[24px] text-[2vw] text-white/40">
+            The platform for minting, buying, and selling NFTs
+          </p>
+          <span className="tablet:size-[500px] absolute left-[-450px] top-[-400px] size-[300px] rounded-full bg-green-400 blur-[450px]" />
         </div>
-        <div className="relative flex w-[410px] items-center">
+        <div className="mobile:w-[410px] relative flex h-[420px] w-[95vw] items-center">
           <HomeNftCard picture={Nft1} size="large" classname="absolute z-[3]" />
           <HomeNftCard picture={Nft2} size="medium" classname="absolute ml-[15%] z-[2]" />
           <HomeNftCard picture={Nft3} size="small" classname="absolute ml-[50%] z-[1]" />
@@ -41,8 +45,8 @@ export const HomeHeaderSection = () => {
           freeMode
           loop
         >
-          {POPULAR_NFT.map((nft) => (
-            <SwiperSlide key={nft.name}>
+          {POPULAR_NFT.map((nft, key) => (
+            <SwiperSlide key={key}>
               <PopularNftCard picture={nft.picture} name={nft.name} minter={nft.minter} like={nft.like} />
             </SwiperSlide>
           ))}
