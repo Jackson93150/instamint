bfd480279b7294a56a7eeaf9221f5d1645c78c72
Jackson93150 <jackjack93150@gmail.com>
2024-03-15 12:12:58 +0100

    feat: add gods ray in homepage

--- front/src/pages/home/home-page.tsx
+++ front/src/pages/home/home-page.tsx
@@ -1,7 +1,21 @@
-import { HomeHeaderSection } from '@/components';
+import { Canvas } from '@react-three/fiber';
+
+import { HomeHeaderSection, GodRays } from '@/components';
 export const HomePage = () => {
   return (
     <div className="flex h-fit w-screen flex-col">
+      <div className="absolute left-0 top-0 z-0 h-screen w-full">
+        <Canvas
+          dpr={1}
+          camera={{ position: [0, 0, 3] }}
+          gl={{
+            powerPreference: 'high-performance',
+          }}
+          linear
+        >
+          <GodRays />
+        </Canvas>
+      </div>
       <HomeHeaderSection />
     </div>
   );
