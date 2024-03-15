cc0caffe61d516d4ae22d6b9b83dc461243c52ef
Mohamed Dhia Ben Ahmed <benahmed.meddhia@gmail.com>
2024-03-16 00:24:45 +0100

    fix: Eslint error fix

--- front/src/services/api/minter.ts
+++ front/src/services/api/minter.ts
@@ -1,31 +1,14 @@
 import axios from 'axios';
 
-export interface Minter {
-  id: number;
+interface MinterRegister {
   username: string;
   email: string;
   password: string;
-  phone?: string | null;
-  bio?: string | null;
-  pictureUrl?: string | null;
-  uniqueUrl?: string | null;
-  isPrivate: boolean;
-  twoFactorEnabled: boolean;
-  twoFactorSecret?: string | null;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
-export const createMinter = async (minter: Minter) => {
+export const createMinter = async (minter: MinterRegister) => {
   const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/minter`, minter);
   return response.data;
-};
-
-export const connectedMinter = async () => {
-  const minter = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/me`, {
-    withCredentials: true,
-  });
-  return minter;
 };
 
 export const updateUniqueUrl = async (uniqueUrl: string) => {
