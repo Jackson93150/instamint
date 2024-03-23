e1986c2e52f64904ce09194edbd27489caa12465
Jackson93150 <jackjack93150@gmail.com>
2024-03-23 10:42:08 +0100

    feat: add animation to sidebar + add nav item

--- front/src/components/sidebar/sidebar.tsx
+++ front/src/components/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import ChatIcon from '@mui/icons-material/Chat';
 import GroupsIcon from '@mui/icons-material/Groups';
 import HomeIcon from '@mui/icons-material/Home';
+import PermMediaIcon from '@mui/icons-material/PermMedia';
 import PersonIcon from '@mui/icons-material/Person';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useContext } from 'react';
@@ -15,7 +16,7 @@ export const Sidebar = () => {
   const minterData = sidebarContext.minterData;
 
   return (
-    <div className="z-sidebar py-10U px-5U gap-10U mobile:w-[450px] fixed right-0 top-0 flex h-screen w-full flex-col bg-green-100">
+    <div className="gsapSidebar z-sidebar py-10U px-5U gap-10U mobile:w-[450px] fixed right-0 top-0 flex h-screen w-full translate-x-full flex-col bg-green-100">
       <Close
         className="top-5U right-5U size-5U absolute cursor-pointer"
         onClick={() => {
@@ -30,10 +31,11 @@ export const Sidebar = () => {
         </div>
       </div>
       <div className="flex size-full flex-col justify-between">
-        <div className="flex flex-col">
+        <div className="gap-1U flex flex-col">
           <NavItem icon={<HomeIcon fontSize="large" />} title="Home" path="/" />
           <NavItem icon={<PersonIcon fontSize="large" />} title="Profile" path="/profile" />
           <NavItem icon={<GroupsIcon fontSize="large" />} title="Tea Bag" path="/minter/tea-bag" />
+          <NavItem icon={<PermMediaIcon fontSize="large" />} title="Content Manager" path="/content-manager" />
           <NavItem icon={<ChatIcon fontSize="large" />} title="Message" path="/messages" />
           <NavItem icon={<SettingsIcon fontSize="large" />} title="Settings" path="/settings" />
         </div>
--- front/src/context/sidebar.context.tsx
+++ front/src/context/sidebar.context.tsx
@@ -1,9 +1,14 @@
+import { useGSAP } from '@gsap/react';
+import cx from 'classnames';
+import gsap from 'gsap';
 import { createContext, useState, ReactNode, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { Sidebar } from '@/components';
 import { MinterInterface } from '@/interfaces';
 import { connectedMinter } from '@/services';
+
+gsap.registerPlugin(useGSAP);
 
 interface SidebarContextType {
   isOpen: boolean;
@@ -28,7 +33,20 @@ export const SidebarProvider = ({ children }: Props) => {
   const [minterData, setMinterData] = useState(null);
   const location = useLocation();
 
+  const { contextSafe } = useGSAP();
+
+  const closeSidebar = contextSafe(() => {
+    gsap.to('.gsapSidebarBlur', { opacity: 0, display: 'none' });
+    gsap.to('.gsapSidebar', { x: '100%', display: 'none' });
+  });
+
+  const openSidebar = contextSafe(() => {
+    gsap.to('.gsapSidebarBlur', { opacity: 1, display: 'block' });
+    gsap.to('.gsapSidebar', { x: '0%', display: 'flex' });
+  });
+
   useEffect(() => {
+    closeSidebar();
     if (!minterData) {
       const fetchMinter = async () => {
         const result = await connectedMinter();
@@ -36,10 +54,16 @@ export const SidebarProvider = ({ children }: Props) => {
       };
       fetchMinter();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location.pathname, minterData]);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
+    if (isOpen) {
+      closeSidebar();
+    } else {
+      openSidebar();
+    }
   };
 
   const contextValue: SidebarContextType = {
@@ -50,7 +74,13 @@ export const SidebarProvider = ({ children }: Props) => {
 
   return (
     <SidebarContext.Provider value={contextValue}>
-      {isOpen && <Sidebar />}
+      <span
+        className={cx(
+          'gsapSidebarBlur',
+          'left-O z-navbar fixed top-0 h-screen w-full bg-black/30 backdrop-blur-[15px]'
+        )}
+      />
+      <Sidebar />
       {children}
     </SidebarContext.Provider>
   );
